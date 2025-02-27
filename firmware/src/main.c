3558589d2047f04606603b3d156ddea143223c66
Martin Pietryka <martin@pietryka.at>
2025-02-27 23:12:07 +0100

    firmware: basic logging setup
    Signed-off-by: Martin Pietryka <martin@pietryka.at>

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -1,7 +1,15 @@
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
 
-
+LOG_MODULE_REGISTER(theadmouse, CONFIG_THEADMOUSE_LOG_LEVEL);
 
 int main(void)
 {
+	LOG_INF("Tina was here ^_^");
+
+	while (true) {
+		k_msleep(100);
+	}
+
 	return 0;
 }
