8776598aec9689af8352fa943711524085413655
Martin Pietryka <martin@pietryka.at>
2025-04-12 23:58:42 +0200

    firmware: first simple HID over GATT (HOG) support
    This is mostly based on the example provided by Zephyr.
    
    Signed-off-by: Martin Pietryka <martin@pietryka.at>

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -1,20 +1,81 @@
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/gatt.h>
+#include <zephyr/bluetooth/uuid.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/flash.h>
 #include <zephyr/drivers/retained_mem.h>
 #include <zephyr/kernel.h>
+#include <zephyr/settings/settings.h>
 #include <zephyr/shell/shell.h>
 #include <zephyr/storage/flash_map.h>
 #include <zephyr/sys/reboot.h>
 #include <zephyr/logging/log.h>
 
+#include "hog.h"
+
 LOG_MODULE_REGISTER(theadmouse, CONFIG_THEADMOUSE_LOG_LEVEL);
+
+static const struct bt_data le_adv[] = {
+	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+	BT_DATA_BYTES(BT_DATA_UUID16_ALL,
+			BT_UUID_16_ENCODE(BT_UUID_HIDS_VAL),
+			BT_UUID_16_ENCODE(BT_UUID_BAS_VAL)),
+};
+
+static const struct bt_data le_scan_rsp[] = {
+	BT_DATA(BT_DATA_NAME_COMPLETE, CONFIG_BT_DEVICE_NAME, sizeof(CONFIG_BT_DEVICE_NAME) - 1),
+};
+
+int ble_init(void)
+{
+	int ret;
+
+	ret = bt_enable(NULL);
+	if (ret < 0) {
+		LOG_ERR("Failed to enable BT: %d", ret);
+		return ret;
+	}
+
+	ret = settings_load_subtree("bt");
+	if (ret < 0) {
+		LOG_ERR("Failed to load 'bt' settings subtree");
+		return ret;
+	}
+
+	ret = hog_init();
+	if (ret < 0) {
+		LOG_ERR("Failed to initialize HOG: %d", ret);
+		return ret;
+	}
+
+	// TODO: Check what `BT_LE_ADV_CONN_ONE_TIME` actually does and if it's the right thing for us
+	ret = bt_le_adv_start(BT_LE_ADV_CONN_ONE_TIME, le_adv, ARRAY_SIZE(le_adv), le_scan_rsp, ARRAY_SIZE(le_scan_rsp));
+	if (ret < 0) {
+		LOG_ERR("Failed to start BLE advertisement: %d", ret);
+		return ret;
+	}
+
+	LOG_INF("Started BLE advertisement, device name: %s", CONFIG_BT_DEVICE_NAME);
+
+	return 0;
+}
 
 int main(void)
 {
+	int ret;
+
 	LOG_INF("Tina was here ^_^");
 
+	ret = ble_init();
+	if (ret < 0) {
+		LOG_ERR("BLE initialization failed");
+	}
+
 	while (true) {
-		k_msleep(100);
+		hog_push_report(0, 100, 0);
+		k_msleep(1000);
+		hog_push_report(0, -100, 0);
+		k_msleep(1000);
 	}
 
 	return 0;
