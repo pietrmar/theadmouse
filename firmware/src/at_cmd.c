b7448ea2aab21c4a8da8d64079649b183f8663cf
Martin Pietryka <martin@pietryka.at>
2025-11-07 12:21:26 +0100

    firmware: at_cmd: make necessary types and helpers public needed for `at_dispatch_cmd()`
    Signed-off-by: Martin Pietryka <martin@pietryka.at>

--- firmware/src/at_cmd.c
+++ firmware/src/at_cmd.c
@@ -18,56 +18,6 @@ LOG_MODULE_REGISTER(at, LOG_LEVEL_DBG);
 
 #define AT_EOL "\r\n"
 
-enum at_param_type {
-	AT_PARAM_NONE = 0,
-	AT_PARAM_INT,
-	AT_PARAM_UINT,
-	AT_PARAM_STR,
-};
-
-struct at_cmd_param {
-	enum at_param_type type;
-
-	union {
-		int32_t	i;
-		uint32_t ui;
-		char *s;
-	} val;
-};
-
-static inline int at_param_get_int(const struct at_cmd_param *p, int32_t *out)
-{
-	if (!p || !out) return -ENODATA;
-	if (p->type != AT_PARAM_INT) return -EINVAL;
-	*out = p->val.i;
-	return 0;
-}
-
-static inline int at_param_get_uint(const struct at_cmd_param *p, uint32_t *out)
-{
-	if (!p || !out) return -ENODATA;
-	if (p->type != AT_PARAM_UINT) return -EINVAL;
-	*out = p->val.ui;
-	return 0;
-}
-
-static inline int at_param_get_str(const struct at_cmd_param *p, char **out)
-{
-	if (!p || !out) return -ENODATA;
-	if (p->type != AT_PARAM_STR) return -EINVAL;
-	*out = p->val.s;
-	return 0;
-}
-
-typedef int (*at_cmd_fn)(const struct at_cmd_param *arg, void *ctx);
-
-struct at_cmd {
-	const char *name;
-	enum at_param_type param_type;
-	at_cmd_fn cb;
-	void *ctx;
-};
-
 int at_cmd_ID(const struct at_cmd_param *arg, void *ctx)
 {
 	at_replyf("%s %s PressureSensor=None ForceSensor=None", CONFIG_APP_PROJECT_NAME, APP_VERSION_STRING);
--- firmware/src/at_cmd.h
+++ firmware/src/at_cmd.h
@@ -1,6 +1,57 @@
 #pragma once
 
+enum at_param_type {
+	AT_PARAM_NONE = 0,
+	AT_PARAM_INT,
+	AT_PARAM_UINT,
+	AT_PARAM_STR,
+};
+
+struct at_cmd_param {
+	enum at_param_type type;
+
+	union {
+		int32_t	i;
+		uint32_t ui;
+		char *s;
+	} val;
+};
+
+typedef int (*at_cmd_fn)(const struct at_cmd_param *arg, void *ctx);
+
+struct at_cmd {
+	const char *name;
+	enum at_param_type param_type;
+	at_cmd_fn cb;
+	void *ctx;
+};
+
 int at_handle_line(char *s, uint16_t len);
+int at_dispatch_cmd(const struct at_cmd *cmd, struct at_cmd_param *param);
 
 int at_reply(const char *s);
 int at_replyf(const char *fmt, ...);
+
+static inline int at_param_get_int(const struct at_cmd_param *p, int32_t *out)
+{
+	if (!p || !out) return -ENODATA;
+	if (p->type != AT_PARAM_INT) return -EINVAL;
+	*out = p->val.i;
+	return 0;
+}
+
+static inline int at_param_get_uint(const struct at_cmd_param *p, uint32_t *out)
+{
+	if (!p || !out) return -ENODATA;
+	if (p->type != AT_PARAM_UINT) return -EINVAL;
+	*out = p->val.ui;
+	return 0;
+}
+
+static inline int at_param_get_str(const struct at_cmd_param *p, char **out)
+{
+	if (!p || !out) return -ENODATA;
+	if (p->type != AT_PARAM_STR) return -EINVAL;
+	*out = p->val.s;
+	return 0;
+}
