9bbb2085dd19c751c3f5fc22ae93ce7573e2952d
Martin Pietryka <martin@pietryka.at>
2025-12-02 02:16:16 +0100

    firmware: MadgwickAHRS: make sampleFreq configurable
    Signed-off-by: Martin Pietryka <martin@pietryka.at>

--- firmware/src/MadgwickAHRS/MadgwickAHRS.c
+++ firmware/src/MadgwickAHRS/MadgwickAHRS.c
@@ -21,12 +21,12 @@
 //---------------------------------------------------------------------------------------------------
 // Definitions
 
-#define sampleFreq	512.0f		// sample frequency in Hz
 #define betaDef		0.1f		// 2 * proportional gain
 
 //---------------------------------------------------------------------------------------------------
 // Variable definitions
 
+volatile float sampleFreq = 512.0f;	// sample frequency in Hz
 volatile float beta = betaDef;								// 2 * proportional gain (Kp)
 volatile float q0 = 1.0f, q1 = 0.0f, q2 = 0.0f, q3 = 0.0f;	// quaternion of sensor frame relative to auxiliary frame
 
