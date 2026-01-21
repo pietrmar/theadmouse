a569109c82db97d3aa66afeece66807fa0cbf7c4
Martin Pietryka <martin@pietryka.at>
2026-01-21 05:20:57 +0100

    firmware: headmouse_input: add support for selecting a keyboard layout
    Signed-off-by: Martin Pietryka <martin@pietryka.at>

--- firmware/src/headmouse_input.c
+++ firmware/src/headmouse_input.c
@@ -1,7 +1,10 @@
 #include <stdbool.h>
+#include <string.h>
 
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+
+#include "keymaps/KeyboardLayout.h"
 
 #include "ble_hid_service.h"
 #include "hm_hid_evt.h"
@@ -89,30 +92,79 @@ int hm_input_report_mouse_btn(enum hm_hid_mouse_btn btn, bool pressed, k_timeout
 	return ble_hid_queue_evt(&evt, timeout);
 }
 
+struct kbd_layout_entry {
+	const char *locale;
+	const uint8_t *layout;
+};
+
+static const struct kbd_layout_entry kbd_layout_table[] = {
+	{ .locale = "da_DK", .layout = KeyboardLayout_da_DK },
+	{ .locale = "de_DE", .layout = KeyboardLayout_de_DE },
+	{ .locale = "en_US", .layout = KeyboardLayout_en_US },
+	{ .locale = "es_ES", .layout = KeyboardLayout_es_ES },
+	{ .locale = "fr_FR", .layout = KeyboardLayout_fr_FR },
+	{ .locale = "hu_HU", .layout = KeyboardLayout_hu_HU },
+	{ .locale = "it_IT", .layout = KeyboardLayout_it_IT },
+	{ .locale = "pt_BR", .layout = KeyboardLayout_pt_BR },
+	{ .locale = "pt_PT", .layout = KeyboardLayout_pt_PT },
+	{ .locale = "sv_SE", .layout = KeyboardLayout_sv_SE },
+};
+
+// The default is `en_US`
+static const struct kbd_layout_entry *current_kbd_layout = &kbd_layout_table[2];
+static struct k_spinlock current_kbd_layout_lock;
+
+const char *hm_input_get_current_kbd_layout_locale(void)
+{
+	k_spinlock_key_t key = k_spin_lock(&current_kbd_layout_lock);
+	const char *locale = current_kbd_layout->locale;
+	k_spin_unlock(&current_kbd_layout_lock, key);
+
+	return locale;
+}
+
+int hm_input_set_kbd_layout_locale(const char *locale)
+{
+	for (size_t i = 0; i < ARRAY_SIZE(kbd_layout_table); i++) {
+		if (strcmp(locale, kbd_layout_table[i].locale) == 0) {
+			k_spinlock_key_t key = k_spin_lock(&current_kbd_layout_lock);
+			current_kbd_layout = &kbd_layout_table[i];
+			k_spin_unlock(&current_kbd_layout_lock, key);
+			return 0;
+		}
+	}
+
+	return -ENOENT;
+}
+
 struct hid_keystroke {
 	uint8_t code;
 	bool shift;
+	bool altgr;
 };
 
-struct hid_keystroke ascii_to_hid_keystroke(char c)
+struct hid_keystroke ascii_to_hid_keystroke(unsigned char c)
 {
-	struct hid_keystroke keystroke = { .code = 0, .shift = false };
+	struct hid_keystroke keystroke = { .code = 0, .shift = false, .altgr = false };
 
-	if (c >= 'a' && c <= 'z') {
-		keystroke.code = (c - 'a') + HID_KEY_A;
-	} else if (c >= 'A' && c <= 'Z') {
-		keystroke.code = (c - 'A') + HID_KEY_A;
+	if (c >= 128)
+		return keystroke;
+
+	uint8_t k = current_kbd_layout->layout[c];
+
+	if ((k & ALT_GR) == ALT_GR) {
+		keystroke.altgr = true;
+		k &= 0x3f;
+	} else if ((k & SHIFT) == SHIFT) {
 		keystroke.shift = true;
-	} else if (c >= '1' && c <= '9') {
-		keystroke.code = (c - '1') + HID_KEY_1;
-	} else {
-		switch (c) {
-			case '0': keystroke.code = HID_KEY_0;
-			default:
-				LOG_WRN("Cannot convert %c to HID code", c);
-				break;
-		}
+		k &= 0x7f;
 	}
+
+	if (k == ISO_REPLACEMENT) {
+		k = ISO_KEY;
+	}
+
+	keystroke.code = k;
 
 	return keystroke;
 }
@@ -120,7 +172,9 @@ struct hid_keystroke ascii_to_hid_keystroke(char c)
 int hm_input_write_string(const char *s)
 {
 	bool shift_active = false;
+	bool altgr_active = false;
 
+	// TODO: Error handling on errors from `hm_input_report_key()`
 	while (*s) {
 		struct hid_keystroke keystroke = ascii_to_hid_keystroke(*s);
 
@@ -133,9 +187,15 @@ int hm_input_write_string(const char *s)
 			k_sleep(K_MSEC(10));
 		}
 
-		// TODO: Error handling
+		if (keystroke.altgr != altgr_active) {
+			altgr_active = keystroke.altgr;
+			hm_input_report_key(HID_KEY_MOD_RALT, altgr_active, K_NO_WAIT);
+			k_sleep(K_MSEC(10));
+		}
+
 		hm_input_report_key(keystroke.code, true, K_NO_WAIT);
 		k_sleep(K_MSEC(10));
+
 		hm_input_report_key(keystroke.code, false, K_NO_WAIT);
 		k_sleep(K_MSEC(10));
 
@@ -145,6 +205,11 @@ int hm_input_write_string(const char *s)
 
 	if (shift_active) {
 		hm_input_report_key(HID_KEY_MOD_LSHIFT, false, K_NO_WAIT);
+		k_sleep(K_MSEC(10));
+	}
+
+	if (altgr_active) {
+		hm_input_report_key(HID_KEY_MOD_RALT, false, K_NO_WAIT);
 		k_sleep(K_MSEC(10));
 	}
 
--- firmware/src/headmouse_input.h
+++ firmware/src/headmouse_input.h
@@ -7,4 +7,7 @@ int hm_input_report_mouse_move(int16_t dx, int16_t dy, k_timeout_t timeout);
 int hm_input_report_mouse_wheel(int8_t steps, k_timeout_t timeout);
 int hm_input_report_mouse_btn(enum hm_hid_mouse_btn btn, bool pressed, k_timeout_t timeout);
 
+const char *hm_input_get_current_kbd_layout_locale(void);
+int hm_input_set_kbd_layout_locale(const char *locale);
+
 int hm_input_write_string(const char *s);
