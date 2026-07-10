77722bc58f78f41ed67a0ea4b45ad887939ba4b9
Martin Pietryka <martin@pietryka.at>
2026-07-10 07:32:54 +0200

    firmware: motion_engine: start reworking
    Signed-off-by: Martin Pietryka <martin@pietryka.at>

--- firmware/src/motion_engine.c
+++ firmware/src/motion_engine.c
@@ -1,17 +1,14 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/logging/log.h>
-#include <zephyr/sys/byteorder.h>
 #include <zephyr/input/input.h>
 #include <zephyr/pm/device.h>
-
-#include <zsl/orientation/orientation.h>
+#include <math.h>
 
 #include "common.h"
 
 #include "headmouse_input.h"
 #include "motion_engine.h"
-#include "telemetry_uart.h"
 
 #include "MadgwickAHRS/MadgwickAHRS.h"
 
@@ -32,7 +29,7 @@ static const struct device *const dev_mag = NULL;
 
 // Possible values for the LSM6DSL from the datasheet:
 //   1.6, 12.5, 26, 52, 104, 208, 416, 833, 1666, 3332, 6664
-#define IMU_SAMPLING_FREQUENCY	416.0f
+#define IMU_SAMPLING_FREQUENCY	208.0f
 
 // Posible values for the LIS2MDL from the datasheet:
 //   10, 20, 50, 100
@@ -65,132 +62,36 @@ static float imu_accum_x = 0.0f;
 static float imu_accum_y = 0.0f;
 static struct k_spinlock imu_accum_lock;
 
-#ifdef CONFIG_TELEMETRY
-
-static inline int feed_telemetry(uint32_t sample_id, const struct zsl_vec *a, const struct zsl_vec *g, const struct zsl_vec *m, const struct zsl_quat *q, float t)
-{
-#if !IS_ENABLED(CONFIG_TELEMETRY_PLAINTEXT)
-#define TELEMETRY_PAYLOAD_LEN	60	// 1*4 (uint32_t) + 14*4 (float)
-
-	//  1 * 4 bytes (uint32_t), sample_id
-	// 14 * 4 bytes (float), a(3), g(3), m(3), q(4), t(1)
-	const size_t telemetry_payload_len = 60;
-	// 2 (sync) + 1 (version) + 2 (length) + payload + 1 (checksum)
-	const size_t buffer_size = 2 + 1 + 2 + telemetry_payload_len + 1;
-	uint8_t buf[buffer_size];
-	size_t idx = 0;
-
-	// sync bytes
-	buf[idx++] = 0xaa;
-	buf[idx++] = 0x55;
-
-	// Version
-	buf[idx++] = 0x01;
-
-	// Payload length
-	sys_put_le16(TELEMETRY_PAYLOAD_LEN, &buf[idx]);
-	idx += 2;
-
-	// Sample ID
-	sys_put_le32(sample_id, &buf[idx]);
-	idx +=4;
-
-#define PACK_F32(_buf, _idx, _value) do {		\
-	float _v = (_value);				\
-	memcpy(&(_buf)[(_idx)], &_v, sizeof(_v));	\
-	(_idx) += sizeof(_v);				\
-} while (0)
-
-	PACK_F32(buf, idx, a->data[0]);
-	PACK_F32(buf, idx, a->data[1]);
-	PACK_F32(buf, idx, a->data[2]);
-
-	PACK_F32(buf, idx, g->data[0]);
-	PACK_F32(buf, idx, g->data[1]);
-	PACK_F32(buf, idx, g->data[2]);
-
-	PACK_F32(buf, idx, m->data[0]);
-	PACK_F32(buf, idx, m->data[1]);
-	PACK_F32(buf, idx, m->data[2]);
-
-	PACK_F32(buf, idx, q->r);
-	PACK_F32(buf, idx, q->i);
-	PACK_F32(buf, idx, q->j);
-	PACK_F32(buf, idx, q->k);
-
-	PACK_F32(buf, idx, t);
-
-#undef PACK_F32
-
-	// Sanity check before the checksum
-	if (idx != (buffer_size - 1)) {
-		// TODO: Maybe do an assert here
-		return -EFAULT;
-	}
-
-	uint8_t checksum = 0;
-	for (size_t i = 2; i < idx; i++) {
-		checksum = (uint8_t)(checksum + buf[i]);
-	}
-	buf[idx++] = checksum;
-
-	return telemetry_uart_write(buf, idx);
-#else
-	// NOTE: This is slow, like up to 1.9 ms slow, most likely becasue of float formatting
-	return telemetry_uart_printf("S,%u,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f\r\n",
-			sample_id,
-			(double)a->data[0], (double)a->data[1], (double)a->data[2],
-			(double)g->data[0], (double)g->data[1], (double)g->data[2],
-			(double)m->data[0], (double)m->data[1], (double)m->data[2],
-			(double)q->r, (double)q->i, (double)q->j, (double)q->k,
-			(double)t);
-#endif // !IS_ENABLED(CONFIG_TELEMETRY_PLAINTEXT)
-}
-#endif
-
-
 // Posible values from the data-sheet in g:
 //  2, 4, 8, 16
-#define LSM6DSL_ACC_FULL_SCALE	8
+#define LSM6DSL_ACC_FULL_SCALE	2
 
 // Possible values from the data-sheet deg/s:
 //  125, 250, 500, 1000, 2000
-#define LSM6DSL_GYR_FULL_SCALE	1000
+#define LSM6DSL_GYR_FULL_SCALE	500
 
 
+extern int slot_manager_set_rgb_led_color(int r, int g, int b);
 
 static void imu_thread(void *, void *, void *)
 {
 	int ret;
 
-	struct zsl_quat q_prev = {
-		.r = 1.0f, .i = 0.0f, .j = 0.0f, .k = 0.0f
-	};
+	float ax_prev = 0.0f;
+	float ay_prev = 0.0f;
+	float az_prev = 0.0f;
 
-	struct zsl_quat q_curr = {
-		.r = 1.0f, .i = 0.0f, .j = 0.0f, .k = 0.0f
-	};
+	float gx_prev = 0.0f;
+	float gy_prev = 0.0f;
+	float gz_prev = 0.0f;
 
-	// Madgwick setup
-	beta = 0.1;
+	float q_p[4] = { 1.0f, 0.0f, 0.0f, 0.0f };
+
 	sampleFreq = IMU_SAMPLING_FREQUENCY;
 
-
-#ifdef CONFIG_TELEMETRY
-	// For the telemetry stream we need to keep a copy of the last mag value read
-	// from our loop as the MAG_DECIM_FACTOR and CONFIG_TELEMETRY_DECIM_FACTOR are not the
-	// same and "unlocked";
-	bool telemetry_mag_valid = false;
-	ZSL_VECTOR_DEF(telemetry_mag, 3);
-	zsl_vec_init(&telemetry_mag);
-#endif
-
-	ZSL_VECTOR_DEF(gyro_bias, 3);
-	zsl_vec_init(&gyro_bias);
-
-	bool first_position_estimate = true;
-
-	uint32_t sample_idx = 0;
+	const uint32_t log_sample_reset_val = IMU_SAMPLING_FREQUENCY / 4;
+	uint32_t log_sample_counter = log_sample_reset_val;
+	bool is_still = true;
 	while (true) {
 		k_sem_take(&imu_trigger_sem, K_FOREVER);
 
@@ -203,170 +104,52 @@ static void imu_thread(void *, void *, void *)
 			LOG_ERR("Failed to fetch IMU sample: %d", ret);
 			continue;
 		}
-		sample_idx++;
-
-		bool mag_fetched = false;
-		if ((sample_idx % MAG_DECIM_FACTOR) == 0) {
-			ret = sensor_sample_fetch_chan(dev_mag, SENSOR_CHAN_MAGN_XYZ);
-			if (ret < 0) {
-				LOG_ERR("Failed to fetch magnetometer sample: %d", ret);
-			}
-			mag_fetched = true;
-		}
-
-		// TODO: Drop 50 Mag samples or determine stability
-		// TODO: Drop 200 IMU samples or determine stability
 
 		// Fetch accelerometer data
 		struct sensor_value acc_val[3];
 		sensor_channel_get(dev_imu, SENSOR_CHAN_ACCEL_XYZ, acc_val);	// In m/s²
-		ZSL_VECTOR_DEF(raw_acc, 3);
-		raw_acc_vec[0] = sensor_value_to_float(&acc_val[0]);
-		raw_acc_vec[1] = sensor_value_to_float(&acc_val[1]);
-		raw_acc_vec[2] = sensor_value_to_float(&acc_val[2]);
-
-		bool accel_clip = ZSL_ABS(raw_acc_vec[0]) >= accel_clip_limit ||
-				  ZSL_ABS(raw_acc_vec[1]) >= accel_clip_limit ||
-				  ZSL_ABS(raw_acc_vec[2]) >= accel_clip_limit;
-		if (accel_clip)
-			atomic_inc(&accel_clip_count);
 
 		// Fetch gyro data
 		struct sensor_value gyro_val[3];
 		sensor_channel_get(dev_imu, SENSOR_CHAN_GYRO_XYZ, gyro_val);	// In rad/s
-		ZSL_VECTOR_DEF(raw_gyr, 3);
-		raw_gyr_vec[0] = sensor_value_to_float(&gyro_val[0]);
-		raw_gyr_vec[1] = sensor_value_to_float(&gyro_val[1]);
-		raw_gyr_vec[2] = sensor_value_to_float(&gyro_val[2]);
 
-		bool gyro_clip = ZSL_ABS(raw_gyr_vec[0]) >= gyro_clip_limit ||
-				 ZSL_ABS(raw_gyr_vec[1]) >= gyro_clip_limit ||
-				 ZSL_ABS(raw_gyr_vec[2]) >= gyro_clip_limit;
-		if (gyro_clip)
-			atomic_inc(&gyro_clip_count);
+		float ax = sensor_value_to_float(&acc_val[0]);
+		float ay = sensor_value_to_float(&acc_val[1]);
+		float az = sensor_value_to_float(&acc_val[2]);
 
-		ZSL_VECTOR_DEF(raw_mag, 3);
-		if (mag_fetched) {
-			struct sensor_value mag_val[3];
-			sensor_channel_get(dev_mag, SENSOR_CHAN_MAGN_XYZ, mag_val);	// In Gauss
-			raw_mag_vec[0] = sensor_value_to_float(&mag_val[0]);
-			raw_mag_vec[1] = sensor_value_to_float(&mag_val[1]);
-			raw_mag_vec[2] = sensor_value_to_float(&mag_val[2]);
-			mag_fetched = true;
+		float gx = sensor_value_to_float(&gyro_val[0]);
+		float gy = sensor_value_to_float(&gyro_val[1]);
+		float gz = sensor_value_to_float(&gyro_val[2]);
 
-#ifdef CONFIG_TELEMETRY
-			telemetry_mag_vec[0] = raw_mag_vec[0];
-			telemetry_mag_vec[1] = raw_mag_vec[1];
-			telemetry_mag_vec[2] = raw_mag_vec[2];
-			telemetry_mag_valid = true;
-#endif
-		}
+		MadgwickAHRSupdateIMU(gx, gy, gz, ax, ay, az);
 
-		// TODO: Handle clipping somehow
+		float dq3 = q3 * q_p[0] - q0 * q_p[3] - q1 * q_p[2] + q2 * q_p[1];
+		float dq2 = q2 * q_p[0] - q0 * q_p[2] + q1 * q_p[3] - q3 * q_p[1];
 
-		zsl_real_t acc_norm = zsl_vec_norm(&raw_acc);
-		zsl_real_t gyr_norm = zsl_vec_norm(&raw_gyr);
-		// TODO: Make use of the magnetometer
-		zsl_real_t mag_norm = zsl_vec_norm(&raw_mag);
+		float raw_dx = 2.0f * dq3;
+		float raw_dy = 2.0f * dq2;
 
-		bool acc_static = false;
-		if (ZSL_ABS(acc_norm - 9.81f) < 0.5f)
-			acc_static = true;
+		q_p[0] = q0;
+		q_p[1] = q1;
+		q_p[2] = q2;
+		q_p[3] = q3;
 
-		bool gyr_static = false;
-		if (gyr_norm < 0.09f)
-			gyr_static = true;
+		ax_prev = ax; ay_prev = ay; az_prev = az;
+		gx_prev = gx; gy_prev = gy; gz_prev = gz;
 
-		ZSL_VECTOR_DEF(accel, 3);
-		zsl_vec_copy(&accel, &raw_acc);
-		zsl_vec_to_unit(&accel);
-
-		if (acc_static && gyr_static) {
-			const zsl_real_t alpha = (1.0f / IMU_SAMPLING_FREQUENCY) / 2.0f;
-
-			// gyro_bias = gyro_bias * (1.0 - alpha) + raw_gyr * alpha
-
-			// tmp1 = gyro_bias * (1 - alpha)
-			ZSL_VECTOR_DEF(tmp1, 3);
-			zsl_vec_copy(&tmp1, &gyro_bias);
-			zsl_vec_scalar_mult(&tmp1, 1.0f - alpha);
-
-			// tmp2 = raw_gyr * alpha
-			ZSL_VECTOR_DEF(tmp2, 3);
-			zsl_vec_copy(&tmp2, &raw_gyr);
-			zsl_vec_scalar_mult(&tmp2, alpha);
-
-			// gyro_bias = tmp1 + tmp2
-			zsl_vec_add(&tmp1, &tmp2, &gyro_bias);
-		}
-		ZSL_VECTOR_DEF(gyro, 3);
-		zsl_vec_sub(&raw_gyr, &gyro_bias, &gyro);
-
-
-		MadgwickAHRSupdateIMU(gyro_vec[0], gyro_vec[1], gyro_vec[2], accel_vec[0], accel_vec[1], accel_vec[2]);
-
-		q_curr.r = q0;
-		q_curr.i = q1;
-		q_curr.j = q2;
-		q_curr.k = q3;
-
-		// Compute delta rotation:
-		//  q_delta = q_curr * q_prev^(-1)
-		struct zsl_quat q_prev_conj;
-		zsl_quat_conj(&q_prev, &q_prev_conj);
-
-		struct zsl_quat q_delta;
-		zsl_quat_mult(&q_curr, &q_prev_conj, &q_delta);
-
-		q_prev = q_curr;
-
-#if 1
-		struct zsl_euler e;
-		zsl_quat_to_euler(&q_delta, &e);
-		float raw_dx = e.z;
-		float raw_dy = e.y;
-#else
-		// Approximation becuase `sin(x) = x` for small-ish x.
-		float raw_dx = 2.0f * q_delta.k;
-		float raw_dy = 2.0f * q_delta.j;
-#endif
-		// If this was the first position estimate just do not accumulate anything
-		// by setting `raw_dx`, and `raw_dy` to false, becasue on the first iteration
-		// `q_prev` was just set to the identity quaternion.
-		if (first_position_estimate) {
-			first_position_estimate = false;
-			raw_dx = 0.0f;
-			raw_dy = 0.0f;
-		}
 
 		k_spinlock_key_t key = k_spin_lock(&imu_accum_lock);
 		imu_accum_x -= raw_dx;
 		imu_accum_y += raw_dy;
 		k_spin_unlock(&imu_accum_lock, key);
 
-#ifdef CONFIG_TELEMETRY
-		if ((sample_idx % CONFIG_TELEMETRY_DECIM_FACTOR) == 0) {
-			struct sensor_value temp;
-			sensor_channel_get(dev_imu, SENSOR_CHAN_DIE_TEMP, &temp);
-			float t = sensor_value_to_float(&temp);
-
-			if (telemetry_mag_valid) {
-				telemetry_mag_valid = false;
-			} else {
-				telemetry_mag_vec[0] = 0.0f;
-				telemetry_mag_vec[1] = 0.0f;
-				telemetry_mag_vec[2] = 0.0f;
-			}
-
-			feed_telemetry(sample_idx, &raw_acc, &raw_gyr, &telemetry_mag, &q_curr, t);
-		}
-#endif
-
 		int64_t t_end = k_uptime_ticks();
 
-		if ((sample_idx % (uint32_t)IMU_SAMPLING_FREQUENCY) == 0) {
+		log_sample_counter -= 1;
+		if (log_sample_counter == 0) {
+			// LOG_DBG("accel_delta: %f", accel_delta);
 			// LOG_DBG("loop time: %lld us", k_ticks_to_us_near64(t_end - t_start));
-			// LOG_DBG("gyro bias: [%f, %f, %f]", (double)gyro_bias_vec[0], (double)gyro_bias_vec[1], (double)gyro_bias_vec[2]);
+			log_sample_counter = log_sample_reset_val;
 		}
 	}
 }
@@ -602,7 +385,7 @@ static void imu_monitor_cb(struct k_timer *timer)
 	if (trigger_delta == 0) {
 		LOG_ERR("IMU trigger stall, trigger: %u (total: %u), clip: a: %u, g: %u", trigger_delta, trigger_count, accel_clip_cnt, gyro_clip_cnt);
 	} else {
-		LOG_DBG("trigger: %u (total: %u), clip: a: %u, g: %u", trigger_delta, trigger_count, accel_clip_cnt, gyro_clip_cnt);
+		// LOG_DBG("trigger: %u (total: %u), clip: a: %u, g: %u", trigger_delta, trigger_count, accel_clip_cnt, gyro_clip_cnt);
 	}
 }
 K_TIMER_DEFINE(imu_monitor_timer, imu_monitor_cb, NULL);
